f1185e16c10ad830d4c0d5cd712028ac22a1c689
Dontula <uday.dontula@innovasolutions.com>
2025-03-25 17:48:17 +0530

    Changed table and search bar to elements and added navigation

--- templates/Posts/index.php
+++ templates/Posts/index.php
@@ -1,13 +1,10 @@
 <div class="container">
     <h2>Posts from API</h2>
 
-    <!-- Render Search Bar -->
     <?= $this->element('search_bar', ['searchTerm' => $searchTerm]) ?>
 
-    <!-- Render Posts Table -->
     <?= $this->element('posts_table', ['posts' => $paginatedPosts, 'searchTerm' => $searchTerm]) ?>
 
-    <!-- Render Pagination Controls -->
     <?= $this->element('pagination', [
         'searchTerm' => $searchTerm,
         'page' => $page,
